208220006db7bd0987fe3f0dd286840a17af51c3
anshuman <ansh5441@gmail.com>
2022-12-04 17:47:23 +0530

    advent setup

---
+++ advent/2022/day_01/sol_1.py
@@ -0,0 +1,10 @@
+input_file = "advent/2022/day_01/input/input"
+with open(input_file) as f:
+  data = f.read().splitlines()
+  elf_energy = [0]
+  for d in data:
+    if d == "":
+      elf_energy.append(0)
+    else:
+      elf_energy[-1] += int(d)
+print(max(elf_energy))
---
+++ advent/2022/day_01/sol_2.py
@@ -0,0 +1,12 @@
+input_file = "advent/2022/day_01/input/input"
+with open(input_file) as f:
+  data = f.read().splitlines()
+  elf_energy = [0]
+  for d in data:
+    if d == "":
+      elf_energy.append(0)
+    else:
+      elf_energy[-1] += int(d)
+
+top_3 = sorted(elf_energy, reverse=True)[:3]
+print(sum(top_3))
